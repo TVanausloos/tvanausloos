257ce627475c047d681706e55b64c1795146e95d
TVanausloos <thomas.vanausloos@cronos.be>
2022-01-20 20:56:34 +0100

    Thomas: add empty legend component

---
+++ src/app/tools/components/table-legend/table-legend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-legend',
+  templateUrl: './table-legend.component.html',
+  styleUrls: ['./table-legend.component.scss']
+})
+export class TableLegendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableComponent } from './table/table.component';
-import { TableItemComponent } from './table-item/table-item.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { TableComponent } from './components/table/table.component';
+import { TableItemComponent } from './components/table-item/table-item.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { TableLegendComponent } from './components/table-legend/table-legend.component';
 
 @NgModule({
-  declarations: [TableComponent, TableItemComponent, ItemDetailComponent],
+  declarations: [
+    TableComponent,
+    TableItemComponent,
+    ItemDetailComponent,
+    TableLegendComponent,
+  ],
   exports: [TableComponent, ItemDetailComponent],
   imports: [CommonModule],
 })
