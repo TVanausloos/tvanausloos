6782a28251190c3928a35f8911f4bf1bcb7f17c4
TVanausloos <thomas.vanausloos@cronos.be>
2022-01-22 20:04:43 +0100

    Thomas: add some initial testdata

--- src/app/tools/service/tools.service.ts
+++ src/app/tools/service/tools.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ToolSummaryItem } from '../models/tool-summary-item';
 import { Observable, of } from 'rxjs';
 import { ToolDetailItem } from '../models/tool-detail-item';
+import { ToolCategory } from '../models/tool-category';
 
 @Injectable({
   providedIn: 'root',
@@ -16,5 +17,48 @@ export class ToolsService {
   }
 }
 
-const toolSummaryItems: ToolSummaryItem[] = [];
-const toolDetailItems: ToolDetailItem[] = [];
+const toolSummaryItems: ToolSummaryItem[] = [
+  new ToolSummaryItem('1', 'Ja', 'Jasmine', ToolCategory.UNIT_TEST),
+  new ToolSummaryItem('2', 'Ka', 'Karma', ToolCategory.UNIT_TEST),
+  new ToolSummaryItem('3', 'Mo', 'Mocha', ToolCategory.UNIT_TEST),
+  new ToolSummaryItem('4', 'Je', 'Jest', ToolCategory.UNIT_TEST),
+];
+const toolDetailItems: ToolDetailItem[] = [
+  new ToolDetailItem(
+    '1',
+    'Ja',
+    'Jasmine',
+    ToolCategory.UNIT_TEST,
+    'https://jasmine.github.io/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    '2',
+    'Ka',
+    'Karma',
+    ToolCategory.UNIT_TEST,
+    'https://karma-runner.github.io/latest/index.html/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    '3',
+    'Mo',
+    'Mocha',
+    ToolCategory.UNIT_TEST,
+    'https://mochajs.org//index.html/',
+    'fake_location',
+    ''
+  ),
+
+  new ToolDetailItem(
+    '4',
+    'Je',
+    'Jest',
+    ToolCategory.UNIT_TEST,
+    'https://jestjs.io/',
+    'fake_location',
+    ''
+  ),
+];
