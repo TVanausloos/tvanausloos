a1c3b27d56dcd2d1f7f5805caa1062b9577d582e
TVanausloos <thomas.vanausloos@cronos.be>
2022-01-22 19:33:25 +0100

    Thomas: add extra page + modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemDetailComponent } from './tools/components/item-detail/item-detail.component';
+import { OverviewPage } from './tools/page/overview-page/overview.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'frontend-landscape/1', pathMatch: 'full' },
-  { path: 'frontend-landscape/:id', component: ItemDetailComponent },
+  { path: '', redirectTo: 'frontend-landscape', pathMatch: 'full' },
+  {
+    path: 'frontend-landscape',
+    component: OverviewPage,
+    children: [
+      { path: '', redirectTo: 'frontend-landscape/1', pathMatch: 'full' },
+      { path: 'frontend-landscape/:id', component: ItemDetailComponent },
+    ],
+  },
   { path: '**', redirectTo: '/' },
 ];
 
--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -4,6 +4,8 @@ import { TableComponent } from './components/table/table.component';
 import { TableItemComponent } from './components/table-item/table-item.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { TableLegendComponent } from './components/table-legend/table-legend.component';
+import { OverviewPage } from './page/overview-page/overview.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,9 @@ import { TableLegendComponent } from './components/table-legend/table-legend.com
     TableItemComponent,
     ItemDetailComponent,
     TableLegendComponent,
+    OverviewPage,
   ],
   exports: [TableComponent, ItemDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class ToolsModule {}
