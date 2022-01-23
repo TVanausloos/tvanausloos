5f6fd85fbb3f662a929f1c16087e49aa2d0806e7
TVanausloos <thomas.vanausloos@cronos.be>
2022-01-23 16:12:51 +0100

    Thomas: add initial version of all tools

--- src/app/tools/service/tools.service.ts
+++ src/app/tools/service/tools.service.ts
@@ -74,4 +74,211 @@ const toolDetailItems: ToolDetailItem[] = [
     'fake_location',
     ''
   ),
+  new ToolDetailItem(
+    uuid(),
+    'Tm',
+    'Jest Timer Mocks',
+    ToolCategory.COMPONENT_TEST,
+    'https://jestjs.io/docs/timer-mocks',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Rm',
+    'RxJs marbles',
+    ToolCategory.UNIT_TEST,
+    'https://rxjs.dev/guide/testing/marble-testing',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Jm',
+    'Jest marbles',
+    ToolCategory.UNIT_TEST,
+    'https://www.npmjs.com/package/jest-marbles',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'St',
+    'Stryker',
+    ToolCategory.MUTATION_TEST,
+    'https://stryker-mutator.io/docs/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Cy',
+    'Cypress',
+    ToolCategory.E2E_TEST,
+    'https://www.cypress.io/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Pw',
+    'Playwright',
+    ToolCategory.E2E_TEST,
+    'https://playwright.dev/docs/intro/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Pr',
+    'Protractor',
+    ToolCategory.E2E_TEST,
+    '',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Ps',
+    'Playwright-Test-Snapshots',
+    ToolCategory.VISUAL_REGRESSION_TEST,
+    'https://playwright.dev/docs/test-snapshots',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Pu',
+    'Puppeteer',
+    ToolCategory.VISUAL_REGRESSION_TEST,
+    'https://pptr.dev/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Cx',
+    'CypressAxe',
+    ToolCategory.ACCESSIBILITY_TEST,
+    'https://www.npmjs.com/package/cypress-axe/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Jx',
+    'Jest Ace',
+    ToolCategory.ACCESSIBILITY_TEST,
+    'https://www.npmjs.com/package/jest-axe',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Rx',
+    'RxJs',
+    ToolCategory.LIBRARY,
+    'https://rxjs.dev/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'NgRx',
+    'NgRx',
+    ToolCategory.LIBRARY,
+    'https://ngrx.io/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Ht',
+    'HttpClientTestModule',
+    ToolCategory.LIBRARY,
+    'https://angular.io/api/common/http/testing/HttpClientTestingModule',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Jas',
+    'JestAutoSpies',
+    ToolCategory.LIBRARY,
+    'https://www.npmjs.com/package/jest-auto-spies',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Nm',
+    'NgMocks',
+    ToolCategory.LIBRARY,
+    'https://www.npmjs.com/package/ng-mocks',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Sp',
+    'Spectator',
+    ToolCategory.LIBRARY,
+    'https://ngneat.github.io/spectator/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Tl',
+    'Testing library',
+    ToolCategory.LIBRARY,
+    'https://testing-library.com/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Th',
+    'Angular Test Harness',
+    ToolCategory.LIBRARY,
+    'https://material.angular.io/guide/using-component-harnesses',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Sb',
+    'StoryBook',
+    ToolCategory.LIBRARY,
+    'https://storybook.js.org/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Nx',
+    'Nx Monorepo',
+    ToolCategory.LIBRARY,
+    'https://nx.dev/',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'St',
+    'SuperTest',
+    ToolCategory.LIBRARY,
+    'https://www.npmjs.com/package/supertest',
+    'fake_location',
+    ''
+  ),
+  new ToolDetailItem(
+    uuid(),
+    'Ms',
+    'MockServer',
+    ToolCategory.LIBRARY,
+    'https://www.npmjs.com/package/mock-server',
+    'fake_location',
+    ''
+  ),
 ];
